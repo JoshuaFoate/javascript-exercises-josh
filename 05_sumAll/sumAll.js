1a6ffc1212d5c41236787cc2ba6e8904afed9047
Joshua Foate <joshuafoate@gmail.com>
2024-01-08 14:05:41 -0600

    Finished all of the problems and removed the skips from all tests.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+    if (int1 < 0 || int2 < 0) {
+        return 'ERROR';
+    }
+    if (typeof int1 !== "number" || typeof int2 !== "number") {
+        return 'ERROR';
+    }
+    let sum;
+    let largerNum;
+    let smallerNum;
+    if (int1 > int2) {
+        sum = int2;
+        largerNum = int1;
+        smallerNum = int2;
+    }
+    else if (int2 > int1) {
+        sum = int1;
+        largerNum = int2;
+        smallerNum = int1;
+    }
+    for (i = smallerNum + 1; i <= largerNum; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celsius = (fahrenheit - 32) * (5/9);
+  if (celsius === 0) {
+    return 0;
+  }
+  else {
+    return Number(celsius.toFixed(1));
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheit = (celsius * (9/5) + 32)
+  if (fahrenheit === 0) {
+    return 0;
+  }
+  else {
+    return Number(fahrenheit.toFixed(1));
+  }
 };
 
 // Do not edit below this line
