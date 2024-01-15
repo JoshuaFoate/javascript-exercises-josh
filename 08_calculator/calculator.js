9c141b7aefb9eed8c2fb85f73817afba1b9c6e89
Joshua Foate <joshuafoate@gmail.com>
2024-01-14 19:48:36 -0600

    Finished exercises 8-12.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,48 @@
-const add = function() {
-	
+const add = function(a, b) {
+  let sum = a + b;
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	let sum = a - b;
+  return sum;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+  if (array.length === 0) {
+    return 0;
+  }
+  else if (array.length === 1) {
+    return array[0];
+  }
+  else {
+    for (i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+    return sum;
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let product = 1;
+  for (i = 0; i < array.length; i++) {
+    product *= array[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	let power = a**b;
+  return power;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let factorial = 1
+	for (i = a; i > 0; i--) {
+    factorial *= i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    if (a < 0) {
+        return "OOPS";
+    }
+    else if (typeof a === 'string') {
+        Number(a);
+    }
+    let fibonacci = []
+    fibonacci[0] = 0;
+    fibonacci[1] = 1;
+    if (a > 1) {
+        for (let i = 2; i < (a+1); i++){
+            fibonacci[i] = fibonacci[(i - 1)] + fibonacci[(i - 2)];
+        }
+        return fibonacci[a];
+    }
+    return fibonacci[a];
 };
 
 // Do not edit below this line
